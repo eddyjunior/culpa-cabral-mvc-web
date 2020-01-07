088b4a08d8c764fc4078ac269fb7fa93d531baaf
eddyjunior <neggoh@gmail.com>
2020-01-07 13:34:10 -0200

    Adicionar arquivos de projeto.

---
+++ CulpaCabral/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CulpaCabral.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
